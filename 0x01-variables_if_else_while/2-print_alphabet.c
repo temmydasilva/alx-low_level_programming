ab6d7404fb5cfdd8fe731182501459b0bd9a4542
temmydasilva <dasilvacornelius@gmail.com>
2023-09-09 07:37:28 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function of the task
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char (i);
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
