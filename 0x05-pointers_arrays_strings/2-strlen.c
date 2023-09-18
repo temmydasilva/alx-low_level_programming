5fec25f362b70fea7ddc8c457260a037aa4fc0f4
temmydasilva <dasilvacornelius@gmail.com>
2023-09-18 15:39:10 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the lenght of the string
+ * @s: string to be counted
+ * Return: lenght of the string
+ */
+
+int _strlen(char *s)
+
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	return (count);
+}
