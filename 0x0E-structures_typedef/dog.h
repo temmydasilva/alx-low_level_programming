93788fa9cdacbc5998e7a42dd00f619b989d7957
temmydasilva <dasilvacornelius@gmail.com>
2023-10-10 05:59:38 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - dog structure
@@ -14,5 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
