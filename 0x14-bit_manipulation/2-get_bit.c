3cbd122d185ce42454cb18df9b764253c511f5df
temmydasilva <dasilvacornelius@gmail.com>
2023-10-28 07:36:58 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * get_bit - returns the value of a bit at a given index
- * @n: 
+ * @n: number at which to check bit
  * @index: the index, starting from 0 of the bit you want to get
  * Return: the value of the bit at index index or -1 if an error occured
  */
