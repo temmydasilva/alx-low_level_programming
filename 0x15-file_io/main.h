afc4f65e1cf0b27b74485c50205c5dc2668cb94c
temmydasilva <dasilvacornelius@gmail.com>
2023-10-31 09:35:27 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,7 @@ int append_text_to_file(const char *filename, char *text_content);
 void error_98(int f0, char *buffer, char *argv);
 void error_99(int f0, char *buffer, char *argv);
 void error_100(int f0, char *buffer);
+void error_file(int file_from, int file_to, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
